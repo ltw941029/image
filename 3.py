ad280c40646d12b4c838770734bc92e05088a88c
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-20 23:46:17 +0900

    Update 3.py

--- 3.py
+++ 3.py
@@ -3,4 +3,4 @@ from PIL import Image
 
 image8 = Image.open('example.jpg')
 
-st.image(image8, caption='output_format='auto', width=200, clamp=150, output_format='auto)
+st.image(image8, caption='output_format='auto', width=200, clamp=150, channels='RGB')
