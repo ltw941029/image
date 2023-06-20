45bd28b39197ec3bcd097984848d92480151c083
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-21 00:12:06 +0900

    image_integrate.py

--- 3.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st         
-import numpy as np
-from PIL import Image
-
-img = Image.open("example.jpg")
-imgArray = np.asarray(img)
-st.image(imagArray, caption='output_format=auto', width=200)
-
