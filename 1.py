6fbfd4e740b9338088842a25eb2cb6c1aabd3da6
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-20 22:53:33 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from PIL import Image
 
+st.image((10,10,1),caption='단색 이미지')
+st.image((10,10,3),caption='컬러 이미지')
+st.image((10,10,4),caption='RGBA 이미지')
+
+
 image = Image.open('example.jpg')
 
-st.image(image, caption='Sunrise by the mountains')
+st.image(image, caption='로컬 이미지')
