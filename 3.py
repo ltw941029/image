607421af3c52f0af1e53320916cf0ef80177bf44
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-21 00:08:51 +0900

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,5 +4,5 @@ from PIL import Image
 
 img = Image.open("example.jpg")
 imgArray = np.asarray(img)
-st.image(imagArray, caption='output_format='auto', width=200)
+st.image(imagArray, caption='output_format=auto', width=200)
 
