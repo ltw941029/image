f400c98bff52c677f11eb6f823a2cce5d600841b
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-20 23:04:15 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from PIL import Image
 import numpy as np
+from PIL import Image
 
-a=np.array([10,10,1])
+
+a=np.array(10,10,1)
 st.image(a,caption='단색 이미지')
 st.image((10,10,3),caption='컬러 이미지')
 st.image((10,10,4),caption='RGBA 이미지')
