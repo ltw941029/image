075935792b2c1d5358d95d34949e340c0ec2be34
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-21 00:05:44 +0900

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,11 +2,7 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 
-array = np.arange(0, 737280, 1, np.uint8)
-array = np.reshape(array, (1024, 720))
-
-im = Image.fromarray(array)
-im.save("example4.jpeg")
-image = Image.open('example4.jpg')
-st.image(image, caption='output_format='auto', width=200, clamp=150, channels='RGB')
+img = Image.open("example.jpg")
+imgArray = np.asarray(img)
+st.image(imagArray, caption='output_format='auto', width=200)
 
